003221b202dcf0386c1d12e026a3ae13b657dbbf
Isabelle Carrier <carrieri@MacBook-Pro-de-Isabelle.local>
2018-05-29 13:34:20 -0400

    Adding test project

---
+++ UnitTest/Class1.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace UnitTest
+{
+    
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Test()
+        {
+            
+        }
+        
+    }
+}
